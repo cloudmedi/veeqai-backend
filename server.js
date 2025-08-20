609dbd35174d3704cccd57596f40c49b4c137609
cloudmedi <destek@vemlo.tv>
2025-08-20 13:56:06 +0300

    Add production domains to CORS whitelist

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use(cors({
     'http://127.0.0.1:5174',
     'http://localhost:5175',
     'http://127.0.0.1:5175',
+    'https://app.veeq.ai',
+    'https://hub.veeq.ai',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true
